bd24581d88d8de248ca114c8e3e41d40dfe2b574
gunkim0318 <gunkim0318@gmail.com>
2020-01-22 23:28:25 +0900

    로그인 기능 구현 중

--- src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -31,22 +31,31 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+        //어떤 로그인 서비스인지 구분하는 코드, 구글, 네이버 등 구분하기 위해 사용
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
+        //primary key를 의미, 구글은 기본 지원
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
 
+        //세션에 사용자 정보를 저장하기 위한 dto 클래스
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()))
-                )
+          Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+          attributes.getAttributes(),
+          attributes.getNameAttributeKey()
+        );
     }
 
     private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
         return null;
     }
 }
